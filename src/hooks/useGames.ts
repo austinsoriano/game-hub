d74727b1bf95b6b42f9c40c86cd5af9ef62067dd
Austin Soriano <austin.soriano@gmail.com>
2023-06-24 16:06:23 -0700

    Created GameCard that includes game image

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,14 +2,15 @@ import React, { useEffect, useState } from 'react'
 import apiClient from '../services/api-client'
 import { CanceledError } from 'axios'
 
-interface Game {
-    id: number,
-    name: String
+export interface Game {
+    id: number;
+    name: string;
+    background_image: string;
 }
 
 interface FetchGamesResponse {
-    count: number,
-    results: Game[]
+    count: number;
+    results: Game[];
 }
 
 const useGames = () => {
