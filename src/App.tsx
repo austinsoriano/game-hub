c9081fe46edd1990bc610bf01136cf82af901df1
Austin Soriano <austin.soriano@gmail.com>
2023-06-27 00:36:28 -0700

    Adding Platform Selector menu component to filter by Platform

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,52 @@
-import { useState } from 'react';
-import { Grid, GridItem, Show } from '@chakra-ui/react'
-import { Genre } from './hooks/useGenres';
-import NavBar from './components/NavBar';
-import GameGrid from './components/GameGrid';
-import GenreList from './components/GenreList';
+import { useState } from "react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Genre } from "./hooks/useGenres";
+import { Platform } from "./hooks/usePlatforms";
+import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
+import PlatformSelector from "./components/PlatformSelector";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
+    null
+  );
 
   return (
     <Grid
       templateAreas={{
         base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"` // 1024px
+        lg: `"nav nav" "aside main"`, // 1024px
       }}
       templateColumns={{
         base: `1fr`,
-        lg: `200px 1fr` // 1024px
+        lg: `200px 1fr`, // 1024px
       }}
-    >  
-      <GridItem area='nav'>
+    >
+      <GridItem area="nav">
         <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
-        <GridItem area='aside' paddingX={5}>
-          <GenreList selectedGenre={selectedGenre} onSelectedGenre={(genre) => setSelectedGenre(genre)}></GenreList>
+        <GridItem area="aside" paddingX={5}>
+          <GenreList
+            selectedGenre={selectedGenre}
+            onSelectedGenre={(genre) => setSelectedGenre(genre)}
+          ></GenreList>
         </GridItem>
       </Show>
-      <GridItem area='main'>
-        <GameGrid selectedGenre={selectedGenre}></GameGrid>
+      <GridItem area="main">
+        <PlatformSelector
+          selectedPlatform={selectedPlatform}
+          onSelectPlatform={(platform) => setSelectedPlatform(platform)}
+        ></PlatformSelector>
+        <GameGrid
+          selectedPlatform={selectedPlatform}
+          selectedGenre={selectedGenre}
+        ></GameGrid>
       </GridItem>
     </Grid>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,35 +1,42 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import { Genre } from "../hooks/useGenres";
+import { Platform } from "../hooks/usePlatforms";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
 interface Props {
-    selectedGenre: Genre | null;
+  selectedGenre: Genre | null;
+  selectedPlatform: Platform | null;
 }
 
-const GameGrid = ({ selectedGenre }: Props) => {
-    const { data, error, isLoading } = useGames(selectedGenre);
-    const skeletons = [1, 2, 3, 4, 5 ,6];
+const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
+  const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
-    return(
-        <>
-            {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5}} padding='10px' spacing={3}>
-                {isLoading && skeletons.map(skeleton => (
-                    <GameCardContainer key={skeleton}>
-                        <GameCardSkeleton></GameCardSkeleton>
-                    </GameCardContainer>)
-                )}
-                {data.map(game => (
-                    <GameCardContainer key={game.id}>
-                        <GameCard game={game}></GameCard>
-                    </GameCardContainer>)
-                )}
-            </SimpleGrid>
-        </>
-    )
-}
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="10px"
+        spacing={3}
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton></GameCardSkeleton>
+            </GameCardContainer>
+          ))}
+        {data.map((game) => (
+          <GameCardContainer key={game.id}>
+            <GameCard game={game}></GameCard>
+          </GameCardContainer>
+        ))}
+      </SimpleGrid>
+    </>
+  );
+};
 
-export default GameGrid
+export default GameGrid;
