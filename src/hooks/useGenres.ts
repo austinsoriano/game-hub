e2f898c667a149c45f92fc359bbbbf53a4f976dd
Austin Soriano <austin.soriano@gmail.com>
2023-08-03 01:21:20 -0700

    Implemented infinite queries

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,8 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
-import APIClient, { FetchResponse } from "../services/api-client";
-
-const apiClient = new APIClient<Genre>("/genres");
+import APIClient from "../services/api-client";
 
 export interface Genre {
   id: number;
@@ -10,13 +8,14 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenres = () => {
-  return useQuery({
+const apiClient = new APIClient<Genre>("/genres");
+
+const useGenres = () =>
+  useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24 hours
-    initialData: { count: genres.length, results: genres },
+    initialData: { count: genres.length, next: null, results: genres },
   });
-};
 
 export default useGenres;
