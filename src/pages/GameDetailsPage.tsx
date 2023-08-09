f28a038662c982bd8fe92193f5979ecf5dec365e
Austin Soriano <austin.soriano@gmail.com>
2023-08-09 15:57:25 -0700

    Setup initial routing

---
+++ src/pages/GameDetailsPage.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/react";
+import NavBar from "../components/NavBar";
+
+const GameDetailsPage = () => {
+  return <div>GameDetailsPage</div>;
+};
+
+export default GameDetailsPage;
